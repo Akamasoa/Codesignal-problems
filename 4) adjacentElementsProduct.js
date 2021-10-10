e4f555fdc9ca6b61c30fe9490af5330ff6022226
Franco Sebastián Genre <84336216+Akamasoa@users.noreply.github.com>
2021-10-09 21:27:38 -0300

    Update and rename adjacentElementsProduct.js to 4) adjacentElementsProduct.js

---
+++ 4) adjacentElementsProduct.js
@@ -0,0 +1,37 @@
+/*
+Given an array of integers, find the pair of adjacent elements that has the largest product and return that product.
+
+Example
+
+For , the output should be
+.inputArray = [3, 6, -2, -5, 7, 3]adjacentElementsProduct(inputArray) = 21
+
+7 and produce the largest product.3
+
+Input/Output
+
+[execution time limit] 4 seconds (js)
+
+[input] array.integer inputArray
+
+An array of integers containing at least two elements.
+
+Guaranteed constraints:
+,
+.2 ≤ inputArray.length ≤ 10-1000 ≤ inputArray[i] ≤ 1000
+
+[output] integer
+
+The largest product of adjacent elements.
+
+[JavaScript] Syntax Tips
+
+// Prints help message to the console
+// Returns a string
+function helloWorld(name) {
+    console.log("This prints to the console when you Run Tests");
+    return "Hello, " + name;
+}
+*/
+
+

9500950a616ae615c079e747dca3dd2c72bd2f5c
Franco Sebastián Genre <84336216+Akamasoa@users.noreply.github.com>
2021-10-14 22:23:09 -0300

    Update 7) almostIncreasingSequence.js

--- 7) almostIncreasingSequence.js
+++ 7) almostIncreasingSequence.js
@@ -39,4 +39,25 @@ function helloWorld(name) {
 }
 */
 
-
+function almostIncreasingSequence(sequence) {
+let found = false;
+  for (let i = 1 ; i < sequence.length; i++) {
+    if(sequence[i] <= sequence[i-1]) {
+      if(found) {
+        return false;
+      }
+      found = true;
+      
+      if(i === 1 || i + 1 === sequence.length) {
+        continue;
+      }
+      else if (sequence[i] > sequence[i-2]) {
+        sequence[i-1] = sequence[i-2];
+      }
+      else if(sequence[i-1] >= sequence[i+1]) {
+        return false;
+      }
+    }
+  }
+  return true;
+}

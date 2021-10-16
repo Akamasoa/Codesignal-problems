ecd4539698f6dfba7d97e0efce3bdbafb87487d8
Franco Sebastián Genre <84336216+Akamasoa@users.noreply.github.com>
2021-10-16 20:37:49 -0300

    Update 10) commonCharacterCount.js

--- 10) commonCharacterCount.js
+++ 10) commonCharacterCount.js
@@ -38,4 +38,15 @@ function helloWorld(name) {
 }
 */
 
-
+function commonCharacterCount(s1, s2) {
+let count = 0;
+    let str = "";
+    for(let i = 0; i<s1.length ; i++){
+        if(s2.indexOf(s1[i])>-1){  
+            count++;
+            s2=s2.replace(s1[i],'');
+        }
+    }                   
+        
+    return count;
+}

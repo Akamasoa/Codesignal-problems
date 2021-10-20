3382c949e16e332c105eac61b459f468447fde26
Franco Sebastián Genre <84336216+Akamasoa@users.noreply.github.com>
2021-10-19 22:08:29 -0300

    Update 13) reverseInParentheses.js

--- 13) reverseInParentheses.js
+++ 13) reverseInParentheses.js
@@ -39,3 +39,45 @@ function helloWorld(name) {
 }
 */
 
+function reverseInParentheses(s) {
+
+// We keep running the function while 
+    // there's an opening bracket in the string
+    while (s.indexOf("(") !== -1) {
+        s = reverseP(s);
+    }
+    return s;
+
+
+function reverseP(s) {
+    let len = s.length;
+    // We find the innermost/last opening bracket,
+    // and then we're going to find the matching closing bracket,
+    // which is the next closing bracket after the opening bracket
+    let openBracketInd = s.lastIndexOf("(");
+    
+    // The characters before the opening bracket
+    let beforeBracket = s.slice(0, openBracketInd+1);
+    
+    // The characters before the opening bracket
+    let afterBracket = s.slice(openBracketInd+1, len);
+    
+    // To get the index of the closing bracket we add the 
+    // characters before the bracket to the index of the first closing
+    // bracket in the string after the opening bracket
+    let closeBracketInd = beforeBracket.length + afterBracket.indexOf(")");
+    
+    // Once we have the indexes, we're going to slice the string 
+    // to remove the brackets
+    let firstPart = s.slice(0, openBracketInd);
+    let secondPart = s.slice(closeBracketInd+1, len);
+    let middle = s.slice(openBracketInd+1, closeBracketInd);
+    
+    // We reverse the string between the brackets
+    middle = middle.split('').reverse().join('');
+    
+    // And finally we join the parts and return the string
+    return firstPart+middle+secondPart;
+  }
+  return {startIndex, endIndex}
+}

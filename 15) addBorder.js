40146624a3e0054746c02e912a890a5cd7d35d4d
Franco Sebastián Genre <84336216+Akamasoa@users.noreply.github.com>
2021-10-21 21:51:48 -0300

    Update 15) addBorder.js

--- 15) addBorder.js
+++ 15) addBorder.js
@@ -15,3 +15,22 @@ addBorder(picture) = ["*****",
                       "*****"]
 */
 
+function addBorder(picture) {
+ // shift a string of '*' to the array the length of the strings
+
+    let topFrame = '*'.repeat(picture[0].length);
+    picture.unshift(topFrame);
+    //console.log(picture);
+    // add a '*' to the beginning and end of each string
+    for(let i = 0; i < picture.length; i ++){
+        picture[i] = picture[i] + '*';
+        picture[i] = '*' + picture[i];
+    }
+    
+    
+    // push a string of '*' to the array with the same length as the strings
+    let bottomFrame = '*'.repeat(picture[1].length);
+    
+    picture.push(bottomFrame);
+    return picture;
+}

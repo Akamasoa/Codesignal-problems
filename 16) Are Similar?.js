b076ffa801395a59aeeeb32b1008f405feccf066
Franco Sebastián Genre <84336216+Akamasoa@users.noreply.github.com>
2021-10-22 21:33:27 -0300

    Update 16) Are Similar?.js

--- 16) Are Similar?.js
+++ 16) Are Similar?.js
@@ -54,3 +54,19 @@ function helloWorld(name) {
 }
 */
 
+function areSimilar(a, b) {
+	const maxDiff = maxDiffs(a, b);
+	if (maxDiff.length > 2 || maxDiff.length === 1) return false;
+	if (maxDiff.length === 0) return true; // identical
+	const [diff1, diff2] = maxDiff;
+	return a[diff1] === b[diff2] && a[diff2] === b[diff1];
+}
+const maxDiffs = (a, b) => {
+	let maxDiff = [];
+	for (let i = 0; i < a.length; i++) {
+		if (a[i] !== b[i]) {
+			maxDiff.push(i);
+		}
+	}
+	return maxDiff;
+};

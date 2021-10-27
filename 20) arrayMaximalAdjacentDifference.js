f77874f282b449cc347d57afc6220b707c1f6213
Franco Sebastián Genre <84336216+Akamasoa@users.noreply.github.com>
2021-10-26 22:26:29 -0300

    Update 20) arrayMaximalAdjacentDifference.js

--- 20) arrayMaximalAdjacentDifference.js
+++ 20) arrayMaximalAdjacentDifference.js
@@ -30,3 +30,13 @@ function helloWorld(name) {
 }
 */
 
+function arrayMaximalAdjacentDifference(inputArray) {
+var max = 0;
+	for (var i = 0; i < inputArray.length - 1; i++) {
+		var diff = Math.abs(inputArray[i] - inputArray[i + 1]);
+		if (diff > max) {
+			max = diff;
+		}
+	}
+	return max;
+}

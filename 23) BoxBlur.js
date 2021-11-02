f36f9179ce60e314464fb5b74841c3c81dee6ca5
Franco Sebastián Genre <84336216+Akamasoa@users.noreply.github.com>
2021-11-02 19:57:53 -0300

    Update 23) BoxBlur.js

--- 23) BoxBlur.js
+++ 23) BoxBlur.js
@@ -55,4 +55,21 @@ function helloWorld(name) {
 }
 */
 
+function boxBlur(image) {
+let result = [];
+    for (let i = 1; i < image.length - 1; i++) {
+        let row = [];
+        for (let j = 1; j < image[i].length - 1; j++) {
+            let sum = 0;
+            for (let k = -1; k <= 1; k++) {
+                for (let l = -1; l <= 1; l++) {
+                    sum += image[i + k][j + l];
+                }
+            }
+            row.push(Math.floor(sum / 9));
+        }
+        result.push(row);
+    }
+    return result;
+}
 

e401627998e12a3f8f80f7260c97788a50f306ba
Franco Sebastián Genre <84336216+Akamasoa@users.noreply.github.com>
2021-11-03 22:13:07 -0300

    Update 25) ArrayReplace.js

--- 25) ArrayReplace.js
+++ 25) ArrayReplace.js
@@ -1,1 +1,39 @@
+/*
+Given an array of integers, replace all the occurrences of with .elemToReplacesubstitutionElem
 
+Example
+
+For , , and , the output should be
+.inputArray = [1, 2, 1]elemToReplace = 1substitutionElem = 3arrayReplace(inputArray, elemToReplace, substitutionElem) = [3, 2, 3]
+
+Input/Output
+
+[execution time limit] 4 seconds (js)
+
+[input] array.integer inputArray
+
+Guaranteed constraints:
+,
+.0 ≤ inputArray.length ≤ 1040 ≤ inputArray[i] ≤ 109
+
+[input] integer elemToReplace
+
+Guaranteed constraints:
+.0 ≤ elemToReplace ≤ 109
+
+[input] integer substitutionElem
+
+Guaranteed constraints:
+.0 ≤ substitutionElem ≤ 109
+
+[output] array.integer
+
+[JavaScript] Syntax Tips
+
+// Prints help message to the console
+// Returns a string
+function helloWorld(name) {
+    console.log("This prints to the console when you Run Tests");
+    return "Hello, " + name;
+}
+*/

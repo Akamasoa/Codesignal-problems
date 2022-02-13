4503f6b16419ad575c8d851e76160e02f40b5587
Franco Sebastián Genre <84336216+Akamasoa@users.noreply.github.com>
2022-02-12 23:54:38 -0300

    Create 40) longestDigitsPrefix.js

---
+++ 40) longestDigitsPrefix.js
@@ -0,0 +1,29 @@
+/*
+Given a string, output its longest prefix which contains only digits.
+
+Example
+
+For inputString = "123aa1", the output should be
+solution(inputString) = "123".
+
+Input/Output
+
+[execution time limit] 4 seconds (js)
+
+[input] string inputString
+
+Guaranteed constraints:
+3 ≤ inputString.length ≤ 100.
+
+[output] string
+
+[JavaScript] Syntax Tips
+
+// Prints help message to the console
+// Returns a string
+function helloWorld(name) {
+    console.log("This prints to the console when you Run Tests");
+    return "Hello, " + name;
+}
+*/
+

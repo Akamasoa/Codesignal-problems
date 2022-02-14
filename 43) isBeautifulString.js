6cfa685cfa193b50cb962325bcfdfbd4618ae0cf
Franco Sebastián Genre <84336216+Akamasoa@users.noreply.github.com>
2022-02-13 21:24:37 -0300

    Update 43) isBeautifulString.js

--- 43) isBeautifulString.js
+++ 43) isBeautifulString.js
@@ -42,3 +42,6 @@ function helloWorld(name) {
 }
 */
 
+function solution(inputString) {
+    return inputString.split('').sort().join('').replace(/([a-z])\1*/g,(it)=>it.length +',').split(',').slice(0,-1).map(Number).every((el,i,ar)=> (inputString.indexOf(String.fromCharCode(i+'a'.charCodeAt(0)))>=0 &&(i==0 || ar[i]<=ar[i-1])));
+}

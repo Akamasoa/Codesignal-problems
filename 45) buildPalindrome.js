0ab6bd5a3ba5fedb35ce091d0cfa93a3706599ff
Franco Sebastián Genre <84336216+Akamasoa@users.noreply.github.com>
2022-02-17 22:18:50 -0300

    Update 45) buildPalindrome.js

--- 45) buildPalindrome.js
+++ 45) buildPalindrome.js
@@ -29,3 +29,14 @@ function helloWorld(name) {
 }
 */
 
+function solution(st) {
+  var i = 0;
+    var aux;
+    while(st != st.split('').reverse().join('')){
+        aux = st.split('')
+        aux.splice(st.length-i, 0 ,st[i])
+        st = aux.join('');
+        i++;
+    }
+    return st;
+}

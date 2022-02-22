111eb6524f7f2ab2c178ea28e307bc364ecc2080
Franco Sebastián Genre <84336216+Akamasoa@users.noreply.github.com>
2022-02-21 23:08:38 -0300

    Update 50) chessKnight.js

--- 50) chessKnight.js
+++ 50) chessKnight.js
@@ -42,3 +42,60 @@ function helloWorld(name) {
 }
 */
 
+function solution(cell) {
+cell = cell.split('');
+    cell[0] = cell[0].charCodeAt(0) - 'a'.charCodeAt(0) +1;
+    cell[1] = Number(cell[1]);
+    aux = [[1,1], [1,1],[1,1],[1,1]]
+    let horse = 8;
+    if(cell[0] < 3){
+        horse -= aux[0][0];
+        horse -= aux[0][1];
+        aux[0][0] = 0;
+        aux[0][1] = 0;
+    }
+    if(cell[0] < 2) {
+        horse -= aux[1][0];
+        horse -= aux[1][1];
+        aux[1][0] = 0;
+        aux[1][1] = 0;
+    }
+    if(cell[0] > 6){
+        horse -= aux[3][0];
+        horse -= aux[3][1];
+        aux[3][0] = 0;
+        aux[3][1] = 0;
+    }
+    if(cell[0] > 7){
+        horse -= aux[2][0];
+        horse -= aux[2][1];
+        aux[2][0] = 0;
+        aux[2][1] = 0;
+    }
+    if(cell[1] < 3){
+        horse -= aux[1][1];
+        horse -= aux[2][1];
+        aux[1][1] = 0;
+        aux[2][1] = 0;
+    }
+    if(cell[1] < 2){
+        horse -= aux[0][1];
+        horse -= aux[3][1];
+        aux[0][1] = 0;
+        aux[3][1] = 0;
+    }
+    if(cell[1] > 6){
+        horse -= aux[1][0];
+        horse -= aux[2][0];
+        aux[1][0] = 0;
+        aux[2][0] = 0;
+    }
+    if(cell[1] > 7){
+        horse -= aux[0][0];
+        horse -= aux[3][0];
+        aux[0][0] = 0;
+        aux[3][0] = 0;
+    }
+    
+    return horse;
+}

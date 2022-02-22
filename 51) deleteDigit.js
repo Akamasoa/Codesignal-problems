9224b0f8181cb7584186f137827b2b85da8ed592
Franco Sebastián Genre <84336216+Akamasoa@users.noreply.github.com>
2022-02-21 23:09:51 -0300

    Update 51) deleteDigit.js

--- 51) deleteDigit.js
+++ 51) deleteDigit.js
@@ -28,3 +28,7 @@ function helloWorld(name) {
 }
 */
 
+function solution(n) {
+let s = String(n).split('').map(Number)
+    return Math.max(...s.map((el,i)=>{var r = s.slice(); r.splice(i,1); return Number( r.join(''))}));
+}

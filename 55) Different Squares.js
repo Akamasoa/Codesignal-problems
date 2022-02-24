f6cd37fdf51bb3b6d4b5da210510e61d50b4a859
Franco Sebastián Genre <84336216+Akamasoa@users.noreply.github.com>
2022-02-23 21:50:04 -0300

    Update 55) Different Squares.js

--- 55) Different Squares.js
+++ 55) Different Squares.js
@@ -52,3 +52,31 @@ function helloWorld(name) {
 }
 */
 
+function solution(matrix) {
+    let d = [];
+    for(let i = 1;  i < matrix.length; i++) {
+        for (let j = 1; j < matrix[i].length; j++) {
+            if(not(i,j)){
+                d.push([
+                    [
+                        matrix[i-1][j-1],
+                        matrix[i-1][j]
+                    ],[
+                        matrix[i][j-1],
+                        matrix[i][j]
+                    ]
+                ]);
+            }
+        }
+    }
+    
+    function not(i, j) {
+        return d.every(c=>{
+           return c[0][0] !== matrix[i-1][j-1] ||
+               c[0][1] !== matrix[i-1][j] || 
+               c[1][0] !== matrix[i][j-1] ||
+               c[1][1] !== matrix[i][j]
+        });
+    }
+    return d.length;
+}

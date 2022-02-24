2745e87b68db5eeedaaae35021d442d1eb790066
Franco Sebastián Genre <84336216+Akamasoa@users.noreply.github.com>
2022-02-23 21:51:08 -0300

    Update 56) digitsProduct.js

--- 56) digitsProduct.js
+++ 56) digitsProduct.js
@@ -28,3 +28,12 @@ function helloWorld(name) {
 }
 */
 
+function solution(product) {
+for(let i = 1; i<5000; i++) {
+        if(p(i) === product)return i;
+    }
+    function p (k) {
+        return String(k).split('').map(Number).reduce((a,b)=>a*b,1)
+    }
+    return -1;
+}

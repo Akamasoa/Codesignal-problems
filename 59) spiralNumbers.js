55325fe1f8fb492b25a44b8a160a0bc926e70094
Franco Sebastián Genre <84336216+Akamasoa@users.noreply.github.com>
2022-03-02 22:39:44 -0300

    Update 59) spiralNumbers.js

--- 59) spiralNumbers.js
+++ 59) spiralNumbers.js
@@ -29,6 +29,17 @@ function helloWorld(name) {
     console.log("This prints to the console when you Run Tests");
     return "Hello, " + name;
 }
-
 */
 
+function solution(n) {
+let res = Array(n*n).fill(null).map((el,i)=>i+1);
+  let m = [];
+  let i = 0;
+  while(res.length) {
+    m.reverse();
+    m = m.length?m[0].map((col, i) => m.map(row => row[i])) : m;
+    m.unshift(res.splice(-(Math.floor(i/2)+1)));
+    i++;
+  }
+  return m;
+}

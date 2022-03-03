e5a0d2c11a6576fd98c68299a0c0971ba79e1cdb
Franco Sebastián Genre <84336216+Akamasoa@users.noreply.github.com>
2022-03-02 22:35:59 -0300

    Update and rename 57) to 57) File Naming.js

--- 57) File Naming.js
+++ 57) File Naming.js
@@ -30,3 +30,31 @@ function helloWorld(name) {
 }
 */
 
+function solution(names) {
+ let n = [];
+    
+    for(let i=0; i < names.length; i++) {
+        let max = 0;
+        let m = [];
+        let f = n.filter(j=>{
+            let reg = new RegExp('^(?:' + names[i].replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&") + '(?:\\((\\d+)\\))?)$');
+            if(reg.test(j)) {
+                if(reg.exec(j)[1] ==undefined )
+                    m[0] =true;
+                else {
+                    m[Number(reg.exec(j)[1])] = true;
+                }
+            }
+            return reg.test(j);
+        });
+        for(var k = 0; k < m.length; k++) {
+            if(!m[k])
+                break;
+        }
+        if(f.length){
+            names[i] = names[i]+ (k ? ('('+ (k)+')') : '');
+        }
+        n.push(names[i])
+    }
+    return n;
+}

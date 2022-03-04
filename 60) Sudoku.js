581ae8f35997999c3b75d45de9c43f3a2637bcb9
Franco Sebastián Genre <84336216+Akamasoa@users.noreply.github.com>
2022-03-03 22:09:58 -0300

    Update 60) Sudoku.js

--- 60) Sudoku.js
+++ 60) Sudoku.js
@@ -63,3 +63,16 @@ function helloWorld(name) {
 }
 */
 
+function solution(grid) {
+return grid.every((row, i)=>{
+      return row.every((el,j)=>{
+          return grid.every((r,k)=>{
+              return (i == k || el !== grid[k][j]) &&
+                  (j == k || el !== grid[i][k]);
+          });
+      });
+  }) && [0,1,2].every((i)=>[0,1,2].every(j=>{
+      let tmp = grid.slice(i*3,i*3+3).map(r=>r.slice(3*j, 3*j+3));
+      return tmp.reduce((a,b)=>a.concat(b)).filter((el,id,self)=>self.indexOf(el)===id).length === 9;
+  }));
+}

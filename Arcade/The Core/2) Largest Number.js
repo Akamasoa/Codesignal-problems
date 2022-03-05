7ff57f03101422294bbac47604062804ecc33f53
Franco Sebastián Genre <84336216+Akamasoa@users.noreply.github.com>
2022-03-04 22:00:21 -0300

    Create 2) Largest Number.js

---
+++ Arcade/The Core/2) Largest Number.js
@@ -0,0 +1,31 @@
+/*
+Given an integer , return the largest number that contains exactly digits.nn
+
+Example
+
+For , the output should be
+.n = 2solution(n) = 99
+
+Input/Output
+
+[execution time limit] 4 seconds (js)
+
+[input] integer n
+
+Guaranteed constraints:
+.1 ≤ n ≤ 9
+
+[output] integer
+
+The largest integer of length .n
+
+[JavaScript] Syntax Tips
+
+// Prints help message to the console
+// Returns a string
+function helloWorld(name) {
+    console.log("This prints to the console when you Run Tests");
+    return "Hello, " + name;
+}
+*/
+

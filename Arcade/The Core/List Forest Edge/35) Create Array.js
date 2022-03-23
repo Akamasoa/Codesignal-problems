557b6a6a0c5368a58d7d9287faa87a85b74dc7c6
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-03-22 22:48:51 -0300

    Update 35) Create Array.js

--- Arcade/The Core/List Forest Edge/35) Create Array.js
+++ Arcade/The Core/List Forest Edge/35) Create Array.js
@@ -1,1 +1,36 @@
+/*
+Given an integer size, return array of length size filled with 1s.
+
+Example
+
+For size = 4, the output should be createArray(size) = [1, 1, 1, 1].
+
+Input/Output
+
+[execution time limit] 4 seconds (js)
+
+[input] integer size
+
+A positive integer.
+
+Guaranteed constraints:
+1≤size≤1000.
+
+[output] array.integer
+
+[JavaScript (ES6)] Syntax Tips
+
+1
+2
+3
+4
+5
+6
+// Prints help message to the console
+// Returns a string
+function helloWorld(name) {
+console.log("This prints to the console when you Run Tests");
+return "Hello, " + name;
+}
+*/
 

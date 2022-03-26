dec395e875aac343315c58f60ff2fca6cbfbea2d
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-03-26 20:28:27 -0300

    Update 37) First Reverse Try.js

--- Arcade/The Core/List Forest Edge/37) First Reverse Try.js
+++ Arcade/The Core/List Forest Edge/37) First Reverse Try.js
@@ -32,3 +32,11 @@ function helloWorld(name) {
 }
 */
 
+function solution(arr) {
+ if (arr.length < 2) {
+     return arr;
+ }
+  let first = arr.shift();
+  let last = arr.pop();
+  return [last, ...arr, first];
+}

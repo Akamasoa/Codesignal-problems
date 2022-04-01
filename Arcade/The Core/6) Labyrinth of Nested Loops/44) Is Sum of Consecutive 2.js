f9252eecc6c481c99adc128a6971b69fb579718b
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-03-31 22:33:35 -0300

    Update 44) Is Sum of Consecutive 2.js

--- Arcade/The Core/6) Labyrinth of Nested Loops/44) Is Sum of Consecutive 2.js
+++ Arcade/The Core/6) Labyrinth of Nested Loops/44) Is Sum of Consecutive 2.js
@@ -36,3 +36,16 @@ function helloWorld(name) {
 }
 */
 
+function solution(n) {
+let r = 0;
+  for (let i = 2; i < n / 2; i++) {
+    if (i % 2) {
+      if (Math.round(n / i) * i == n && n / i > i / 2)
+        r++;
+    } else {
+      if (Math.floor(n / i) * i != n && (Math.floor(n / i) + Math.ceil(n / i)) == n * 2 / i && n / i > i / 2)
+        r++;
+    }
+  }
+  return r;
+}

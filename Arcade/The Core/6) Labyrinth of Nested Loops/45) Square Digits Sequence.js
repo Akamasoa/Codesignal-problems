a63150f6d792c809a0c0498de79cc097bd32863a
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-04-01 21:11:55 -0300

    Update 45) Square Digits Sequence.js

--- Arcade/The Core/6) Labyrinth of Nested Loops/45) Square Digits Sequence.js
+++ Arcade/The Core/6) Labyrinth of Nested Loops/45) Square Digits Sequence.js
@@ -49,3 +49,13 @@ function helloWorld(name) {
 }
 */
 
+function solution(a0) {
+let h = [a0];
+
+  let n = String(a0).split('').map(Number).map(el => el * el).reduce((a, b) => a + b);
+  while (h.indexOf(n) < 0) {
+    h.push(n);
+    n = String(n).split('').map(Number).map(el => el * el).reduce((a, b) => a + b);
+  }
+  return h.length + 1;
+}

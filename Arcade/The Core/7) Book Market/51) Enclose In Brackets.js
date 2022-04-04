42abeded03f1f3b42fca3495ebbf60dc2e351018
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-04-03 21:22:44 -0300

    Create 51) Enclose In Brackets.js

---
+++ Arcade/The Core/7) Book Market/51) Enclose In Brackets.js
@@ -0,0 +1,34 @@
+/*
+Given a string, enclose it in round brackets.
+
+Example
+
+For inputString = "abacaba", the output should be encloseInBrackets(inputString) = "(abacaba)".
+
+Input/Output
+
+[execution time limit] 4 seconds (js)
+
+[input] string inputString
+
+Guaranteed constraints:
+0 ≤ inputString.length ≤ 10.
+
+[output] string
+
+[JavaScript (ES6)] Syntax Tips
+
+1
+2
+3
+4
+5
+6
+// Prints help message to the console
+// Returns a string
+function helloWorld(name) {
+console.log("This prints to the console when you Run Tests");
+return "Hello, " + name;
+}
+*/
+

21506747245b05bad8eb3bf050d0fcfee7968ea9
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-04-12 22:28:27 -0300

    Update 62) Construct Square.js

--- Arcade/The Core/8) Mirror Lake/62) Construct Square.js
+++ Arcade/The Core/8) Mirror Lake/62) Construct Square.js
@@ -35,3 +35,30 @@ return "Hello, " + name;
 }
 */
 
+function constructSquare(s) {
+  let min = Number(
+    "1" +
+      Array(Math.floor((s.length - 1) / 2))
+        .fill(0)
+        .join("")
+  );
+  let max = Number(
+    Array(Math.ceil(s.length / 2))
+      .fill(9)
+      .join("")
+  );
+  let d = getDigitsCount(s);
+  for (let i = max + 1; i >= min - 1; i--) {
+    if (getDigitsCount(String(i * i)) === d) return i * i;
+  }
+  return -1;
+
+  function getDigitsCount(str) {
+    let d = [];
+    while (str.length) {
+      let c = str[0];
+      d.push(str.length - (str = str.replace(new RegExp(c, "g"), "")).length);
+    }
+    return d.sort((a, b) => b - a).join("");
+  }
+}

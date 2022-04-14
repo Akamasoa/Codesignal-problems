5bf6a8edd3975fbdc9e13854f56ec788e5f69167
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-04-13 23:06:01 -0300

    Update 63) Numbers Grouping.js

--- Arcade/The Core/8) Mirror Lake/63) Numbers Grouping.js
+++ Arcade/The Core/8) Mirror Lake/63) Numbers Grouping.js
@@ -50,3 +50,11 @@ return "Hello, " + name;
 }
 */
 
+function numbersGrouping(a) {
+  let groups = Array(10000).fill(false);
+  for (let i = 0; i < a.length; i++) {
+    groups[Math.floor((a[i] - 1) / 10000)] = true;
+  }
+
+  return a.length + groups.reduce((a, b) => a + b, 0);
+}

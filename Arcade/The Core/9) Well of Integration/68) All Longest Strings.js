3079bb9314d6b975bc09162d1d06d27fa6a5d309
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-04-16 22:42:21 -0300

    Update 68) All Longest Strings.js

--- Arcade/The Core/9) Well of Integration/68) All Longest Strings.js
+++ Arcade/The Core/9) Well of Integration/68) All Longest Strings.js
@@ -36,3 +36,7 @@ return "Hello, " + name;
 }
 */
 
+function allLongestStrings(inputArray) {
+  let maxLength = Math.max(...inputArray.map(s => s.length));
+  return inputArray.filter(s => s.length === maxLength);
+}

cc746bf7b8044a6c97e16c470b64ed339d9243a8
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-04-16 22:41:39 -0300

    Create 68) All Longest Strings.js

---
+++ Arcade/The Core/9) Well of Integration/68) All Longest Strings.js
@@ -0,0 +1,38 @@
+/*
+Given an array of strings, return another array containing all of its longest strings.
+
+Example
+
+For inputArray = ["aba", "aa", "ad", "vcd", "aba"], the output should be allLongestStrings(inputArray) = ["aba", "vcd", "aba"].
+
+Input/Output
+
+[execution time limit] 4 seconds (js)
+
+[input] array.string inputArray
+
+A non-empty array.
+
+Guaranteed constraints:
+5 ≤ inputArray.length ≤ 10,
+0 ≤ inputArray[i] ≤ 10.
+
+[output] array.string
+
+Array of the longest strings, stored in the same order as in the inputArray.
+[JavaScript (ES6)] Syntax Tips
+
+1
+2
+3
+4
+5
+6
+// Prints help message to the console
+// Returns a string
+function helloWorld(name) {
+console.log("This prints to the console when you Run Tests");
+return "Hello, " + name;
+}
+*/
+

04d40c7a10b0c469b4a5f3c37a7aea73269680ce
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-04-19 21:57:46 -0300

    Update 72) Add Border.js

--- Arcade/The Core/9) Well of Integration/72) Add Border.js
+++ Arcade/The Core/9) Well of Integration/72) Add Border.js
@@ -45,3 +45,10 @@ return "Hello, " + name;
 }
 */
 
+function addBorder(picture) {
+  return [Array(picture[0].length + 2).fill('*').join('')]
+    .concat(
+      picture.map(r => '*' + r + '*'),
+      [Array(picture[0].length + 2).fill('*').join('')]
+    )
+}

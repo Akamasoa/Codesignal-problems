dee6b5862827a6165d75064c553223978eeb08e5
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-04-19 21:55:47 -0300

    Create 72) Add Border.js

---
+++ Arcade/The Core/9) Well of Integration/72) Add Border.js
@@ -0,0 +1,47 @@
+/*
+Given a rectangular matrix of characters, add a border of asterisks(\*) to it.
+
+Example
+
+For
+picture = ["abc",
+"ded"]
+
+the output should be
+
+addBorder(picture) = ["*****",
+"*abc*",
+"*ded*",
+"*****"]
+
+Input/Output
+
+[execution time limit] 4 seconds (js)
+
+[input] array.string picture
+
+A non-empty array of non-empty equal-length strings.
+
+Guaranteed constraints:
+1 ≤ picture.length ≤ 100,
+1 ≤ picture[i].length ≤ 100.
+
+[output] array.string
+
+The same matrix of characters, framed with a border of asterisks of width 1.
+[JavaScript (ES6)] Syntax Tips
+
+1
+2
+3
+4
+5
+6
+// Prints help message to the console
+// Returns a string
+function helloWorld(name) {
+console.log("This prints to the console when you Run Tests");
+return "Hello, " + name;
+}
+*/
+

59c389154ccadd10e52804aa502b24556b846554
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-04-22 14:47:31 -0300

    Update 75) Elections Winners.js

--- Arcade/The Core/9) Well of Integration/75) Elections Winners.js
+++ Arcade/The Core/9) Well of Integration/75) Elections Winners.js
@@ -52,3 +52,8 @@ return "Hello, " + name;
 }
 */
 
+function electionsWinners(votes, k) {
+  let max = Math.max(...votes);
+  let numOfMax = votes.filter(v => v === max).length;
+  return votes.reduce((acc, v) => acc + (v === max && numOfMax === 1 || v + k > max ? 1 : 0), 0);
+}

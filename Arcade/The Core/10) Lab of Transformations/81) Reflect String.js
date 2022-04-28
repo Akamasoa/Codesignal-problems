b9f967215e89eba35f914dad9ed0844ecc955a05
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-04-27 21:13:52 -0300

    Update 81) Reflect String.js

--- Arcade/The Core/10) Lab of Transformations/81) Reflect String.js
+++ Arcade/The Core/10) Lab of Transformations/81) Reflect String.js
@@ -38,3 +38,6 @@ return "Hello, " + name;
 }
 */
 
+function reflectString(inputString) {
+  return inputString.split('').map(c => String.fromCharCode('z'.charCodeAt(0) - c.charCodeAt(0) + 'a'.charCodeAt(0))).join('');
+}

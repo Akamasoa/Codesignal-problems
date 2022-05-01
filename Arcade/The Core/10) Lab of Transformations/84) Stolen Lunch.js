745aaeb17143ccfd2d10771831338fd52d85b8d4
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-05-01 20:50:19 -0300

    Update 84) Stolen Lunch.js

--- Arcade/The Core/10) Lab of Transformations/84) Stolen Lunch.js
+++ Arcade/The Core/10) Lab of Transformations/84) Stolen Lunch.js
@@ -37,3 +37,12 @@ return "Hello, " + name;
 }
 */
 
+function stolenLunch(note) {
+  return note.replace(/\d|[a-j]/g, function (char) {
+    if (char >= "a" && char <= "j") {
+      return String.fromCharCode(char.charCodeAt(0) - 'a'.charCodeAt(0) + '0'.charCodeAt(0));
+    } else {
+      return String.fromCharCode(char.charCodeAt(0) - '0'.charCodeAt(0) + 'a'.charCodeAt(0));
+    }
+  });
+}

a8858182e0e43d0b10fb0563b38c5ab83da9c86a
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-05-04 20:18:26 -0300

    Update 88) Array Conversion.js

--- Arcade/The Core/11) Spring of Integration/88) Array Conversion.js
+++ Arcade/The Core/11) Spring of Integration/88) Array Conversion.js
@@ -39,3 +39,16 @@ return "Hello, " + name;
 }
 */
 
+function arrayConversion(inputArray) {
+  let tmp = [];
+  let j = 0;
+  while (inputArray.length > 1) {
+    tmp = [];
+    for (let i = 0; i < inputArray.length; i += 2) {
+      tmp.push(inputArray.slice(i, i + 2));
+    }
+    inputArray = tmp.map(pair => (j ? pair[0] * pair[1] : pair[0] + pair[1]));
+    j = (j + 1) % 2;
+  }
+  return inputArray[0];
+}

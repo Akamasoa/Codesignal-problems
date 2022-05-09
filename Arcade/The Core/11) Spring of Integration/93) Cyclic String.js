fbbb4b39d4152657924248863067c617d1241718
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-05-09 18:52:33 -0300

    Update 93) Cyclic String.js

--- Arcade/The Core/11) Spring of Integration/93) Cyclic String.js
+++ Arcade/The Core/11) Spring of Integration/93) Cyclic String.js
@@ -44,3 +44,21 @@ return "Hello, " + name;
 }
 */
 
+function cyclicString(s) {
+  let arr = s.split("");
+  for (let i = 1; i < arr.length; i++) {
+    if (isSub(arr.slice(0, i), arr.slice(i))) {
+      return i;
+    }
+  }
+  return arr.length;
+}
+
+function isSub(sub, candidate) {
+  for (let i = 0; i < candidate.length; i++) {
+    if (candidate[i] !== sub[i % sub.length]) {
+      return false;
+    }
+  }
+  return true;
+}

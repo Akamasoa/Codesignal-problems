cc705392174ae1a2968c77bbe8b4ea0ef369bcdc
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-05-10 21:07:58 -0300

    Update 94) Beautiful Text.js

--- Arcade/The Core/11) Spring of Integration/94) Beautiful Text.js
+++ Arcade/The Core/11) Spring of Integration/94) Beautiful Text.js
@@ -64,3 +64,46 @@ return "Hello, " + name;
 }
 */
 
+function beautifulText(inputString, l, r) {
+  let copy;
+  let beautiful;
+  let beauLen = [];
+  if (inputString.length < l) {
+    return false;
+  } else if (inputString.length <= r) {
+    return true;
+  }
+  while (l <= r) {
+    let row = 0;
+    beautiful = [[]];
+    copy = inputString.split(" ");
+    beauLen = Array(copy.length).fill(0);
+    while (beauLen[row] < l) {
+      beautiful[row].push(copy.shift());
+      beauLen[row] =
+        beautiful[row].reduce((a, b) => a + b.length, 0) +
+        (beautiful[0].length - 1);
+    }
+    l = beauLen[0] + 1;
+    if (beauLen[0] > r) {
+      return false;
+    }
+    while (copy.length) {
+      row++;
+      beautiful[row] = [];
+      while (copy.length && beauLen[row] < beauLen[row - 1]) {
+        beautiful[row].push(copy.shift());
+        beauLen[row] =
+          beautiful[row].reduce((a, b) => a + b.length, 0) +
+          (beautiful[row].length - 1);
+      }
+      if (beauLen[row] > beauLen[row - 1]) {
+        break;
+      }
+    }
+    if (copy.length === 0 && beauLen[row] === beauLen[row - 1]) {
+      return true;
+    }
+  }
+  return false;
+}

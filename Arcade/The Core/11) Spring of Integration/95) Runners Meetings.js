35788edcc85eecdb446751f982113f077f798c34
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-05-11 22:18:00 -0300

    Update 95) Runners Meetings.js

--- Arcade/The Core/11) Spring of Integration/95) Runners Meetings.js
+++ Arcade/The Core/11) Spring of Integration/95) Runners Meetings.js
@@ -52,3 +52,27 @@ return "Hello, " + name;
 }
 */
 
+var maxMeetings = -1;
+  let meetingPoint;
+  let currentMeeting = -1;
+  for (let i = 0; i < startPosition.length; i++) {
+    for (let j = i + 1; j < startPosition.length; j++) {
+      meetingPoint =
+        (startPosition[i] - startPosition[j]) / (speed[j] - speed[i]);
+      if (meetingPoint >= 0) {
+        currentMeeting = 2;
+        for (let k = j + 1; k < startPosition.length; k++) {
+          if (
+            startPosition[i] + speed[i] * meetingPoint ===
+            startPosition[k] + speed[k] * meetingPoint
+          ) {
+            currentMeeting++;
+          }
+        }
+        if (currentMeeting > maxMeetings) {
+          maxMeetings = currentMeeting;
+        }
+      }
+    }
+  }
+  return maxMeetings;

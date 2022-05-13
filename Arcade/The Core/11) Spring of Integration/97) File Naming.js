63c5caf653c314ad9ef509cffd60923f1c3fbdc2
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-05-13 19:52:43 -0300

    Update 97) File Naming.js

--- Arcade/The Core/11) Spring of Integration/97) File Naming.js
+++ Arcade/The Core/11) Spring of Integration/97) File Naming.js
@@ -1,1 +1,49 @@
+/*
+You are given an array of desired filenames in the order of their creation. Since two files cannot have equal names, the one which comes later will have an addition to its name in a form of (k), where k is the smallest positive integer such that the obtained name is not used yet.
 
+Return an array of names that will be given to the files.
+
+Example
+
+For names = ["doc", "doc", "image", "doc(1)", "doc"], the output should be fileNaming(names) = ["doc", "doc(1)", "image", "doc(1)(1)", "doc(2)"].
+
+Input/Output
+
+[execution time limit] 4 seconds (js)
+
+[input] array.string names
+
+Guaranteed constraints:
+5 ≤ names.length ≤ 1000,
+1 ≤ names[i].length ≤ 15.
+
+[output] array.string
+
+[JavaScript (ES6)] Syntax Tips
+
+1
+2
+3
+4
+5
+6
+// Prints help message to the console
+// Returns a string
+function helloWorld(name) {
+console.log("This prints to the console when you Run Tests");
+return "Hello, " + name;
+}
+*/
+
+function fileNaming(names) {
+  let newNames = [];
+  while (names.length) {
+    let i = 0;
+    let name = names.shift();
+    while (newNames.includes(name + (i ? "(" + i + ")" : ""))) {
+      i++;
+    }
+    newNames.push(name + (i ? "(" + i + ")" : ""));
+  }
+  return newNames;
+}

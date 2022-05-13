4b97a32c748980cf56179acdff1d6f5e2f578dc1
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-05-12 22:45:27 -0300

    Update 96) Christmas Tree.js

--- Arcade/The Core/11) Spring of Integration/96) Christmas Tree.js
+++ Arcade/The Core/11) Spring of Integration/96) Christmas Tree.js
@@ -121,3 +121,30 @@ return "Hello, " + name;
 }
 */
 
+function christmasTree(levelNum, levelHeight) {
+  let maxWidth = 5 + (levelHeight + levelNum - 2) * 2;
+  let tree = [];
+  let crown = [
+    " ".repeat((maxWidth - 1) / 2) + "*",
+    " ".repeat((maxWidth - 1) / 2) + "*",
+    " ".repeat((maxWidth - 3) / 2) + "***"
+  ];
+  let levels = [];
+  let foot = [];
+
+  for (let i = 0; i < levelNum; i++) {
+    for (let j = 0; j < levelHeight; j++) {
+      levels.push(
+        " ".repeat(levelHeight - 1 - j + (levelNum - i - 1)) +
+          "*".repeat(5 + 2 * (j + i))
+      );
+    }
+    foot.push(
+      " ".repeat((maxWidth - 1) / 2 - Math.floor(levelHeight / 2)) +
+        "*".repeat(levelHeight + (levelHeight % 2 === 0 ? 1 : 0))
+    );
+  }
+
+  let tree = crown.concat(levels, foot);
+  return tree;
+}

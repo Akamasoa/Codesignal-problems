cc6e8d6d4ab672918b0de805e5a94dd75e065a01
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-05-15 22:03:53 -0300

    Update 99) Are Isomorphic?.js

--- Arcade/The Core/12) List Backwoods/99) Are Isomorphic?.js
+++ Arcade/The Core/12) List Backwoods/99) Are Isomorphic?.js
@@ -65,3 +65,9 @@ return "Hello, " + name;
 }
 */
 
+function areIsomorphic(array1, array2) {
+  return (
+    array1.length === array2.length &&
+    array1.every((row, i) => row.length === array2[i].length)
+  );
+}

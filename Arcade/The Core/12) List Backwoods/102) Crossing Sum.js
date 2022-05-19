b4036cfa072e472bce334c98eba0ae35834820e4
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-05-18 21:43:12 -0300

    Update 102) Crossing Sum.js

--- Arcade/The Core/12) List Backwoods/102) Crossing Sum.js
+++ Arcade/The Core/12) List Backwoods/102) Crossing Sum.js
@@ -59,3 +59,10 @@ return "Hello, " + name;
 }
 */
 
+function crossingSum(matrix, a, b) {
+  return (
+    matrix[a].reduce((c, d) => c + d) +
+    matrix.reduce((acc, row) => acc + row[b], 0) -
+    matrix[a][b]
+  );
+}

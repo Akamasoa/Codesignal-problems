bbd421532aa6a63f4817e634b71c258eb9a5117d
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-05-19 22:47:37 -0300

    Update 103) Draw Rectangle.js

--- Arcade/The Core/12) List Backwoods/103) Draw Rectangle.js
+++ Arcade/The Core/12) List Backwoods/103) Draw Rectangle.js
@@ -72,3 +72,18 @@ return "Hello, " + name;
 }
 */
 
+function drawRectangle(canvas, rectangle) {
+  canvas[rectangle[1]][rectangle[0]] = "*";
+  canvas[rectangle[3]][rectangle[2]] = "*";
+  canvas[rectangle[1]][rectangle[2]] = "*";
+  canvas[rectangle[3]][rectangle[0]] = "*";
+  for (let i = rectangle[0] + 1; i < rectangle[2]; i++) {
+    canvas[rectangle[1]][i] = "-";
+    canvas[rectangle[3]][i] = "-";
+  }
+  for (let i = rectangle[1] + 1; i < rectangle[3]; i++) {
+    canvas[i][rectangle[0]] = "|";
+    canvas[i][rectangle[2]] = "|";
+  }
+  return canvas;
+}

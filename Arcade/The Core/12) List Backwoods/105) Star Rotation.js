1bfb839b04669f41434b4563d7a3064c201011fc
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-05-21 23:13:58 -0300

    Update 105) Star Rotation.js

--- Arcade/The Core/12) List Backwoods/105) Star Rotation.js
+++ Arcade/The Core/12) List Backwoods/105) Star Rotation.js
@@ -99,3 +99,24 @@ return "Hello, " + name;
 }
 */
 
+function starRotation(matrix, width, center, t) {
+  let rotations = [
+    (i) => [center[0] - i, center[1]],
+    (i) => [center[0] - i, center[1] + i],
+    (i) => [center[0], center[1] + i],
+    (i) => [center[0] + i, center[1] + i],
+    (i) => [center[0] + i, center[1]],
+    (i) => [center[0] + i, center[1] - i],
+    (i) => [center[0], center[1] - i],
+    (i) => [center[0] - i, center[1] - i],
+  ];
+  let rotated = matrix.map((row) => row.slice());
+  t = t % 8;
+  for (let i = 1; i <= (width - 1) / 2; i++) {
+    for (let j = 0; j < 8; j++) {
+      rotated[rotations[(j + t) % 8](i)[0]][rotations[(j + t) % 8](i)[1]] =
+        matrix[rotations[j](i)[0]][rotations[j](i)[1]];
+    }
+  }
+  return rotated;
+}

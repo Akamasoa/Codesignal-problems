8416de734e337c2834a7775c6bb366fc88495b92
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-05-22 20:09:58 -0300

    Update 106) Sudoku.js

--- Arcade/The Core/13) Waterfall of Integration/106) Sudoku.js
+++ Arcade/The Core/13) Waterfall of Integration/106) Sudoku.js
@@ -63,3 +63,26 @@ function helloWorld(name) {
 }
 */
 
+function solution(grid) {
+let ret = true;
+  ret = grid.every((row) => row.every((el, id, arr) => id === arr.indexOf(el)));
+  ret =
+    ret &&
+    grid[0]
+      .map((c, i) => grid.map((_, j) => grid[j][i]))
+      .every((row) => row.every((el, id, arr) => id === arr.indexOf(el)));
+
+  ret =
+    ret &&
+    grid
+      .map((_, i) =>
+        grid.map(
+          (_, j) =>
+            grid[Math.floor(i / 3) * 3 + Math.floor(j / 3)][
+              (i % 3) * 3 + (j % 3)
+            ]
+        )
+      )
+      .every((row) => row.every((el, id, arr) => id === arr.indexOf(el)));
+  return ret;
+}

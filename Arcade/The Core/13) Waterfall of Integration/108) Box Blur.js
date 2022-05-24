0750a5a73f5053c7b6f8051326a045188ffc09a0
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-05-23 23:13:53 -0300

    Update 108) Box Blur.js

--- Arcade/The Core/13) Waterfall of Integration/108) Box Blur.js
+++ Arcade/The Core/13) Waterfall of Integration/108) Box Blur.js
@@ -55,3 +55,25 @@ function helloWorld(name) {
 }
 */
 
+function solution(image) {
+let adjacents = ([i, j]) => [
+    [i, j],
+    [i - 1, j - 1],
+    [i - 1, j],
+    [i - 1, j + 1],
+    [i, j - 1],
+    [i, j + 1],
+    [i + 1, j - 1],
+    [i + 1, j],
+    [i + 1, j + 1],
+  ];
+  let bluried = image.slice(2).map((r) => r.slice(2));
+  for (let i = 1; i < image.length - 1; i++) {
+    for (let j = 1; j < image[i].length - 1; j++) {
+      bluried[i - 1][j - 1] = Math.floor(
+        adjacents([i, j]).reduce((acc, el) => acc + image[el[0]][el[1]], 0) / 9
+      );
+    }
+  }
+  return bluried;
+}

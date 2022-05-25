2d67310ce7a92165d68bc0ca971b0d77973f4d5a
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-05-24 22:28:41 -0300

    Update 109) Contours Shifting.js

--- Arcade/The Core/13) Waterfall of Integration/109) Contours Shifting.js
+++ Arcade/The Core/13) Waterfall of Integration/109) Contours Shifting.js
@@ -77,3 +77,67 @@ function helloWorld(name) {
 }
 */
 
+function solution(matrix) {
+let maxC = Math.floor((Math.min(matrix.length, matrix[0].length) + 1) / 2);
+  let q = [];
+  let next;
+  for (let i = 0; i < maxC; i++) {
+    let it = contourIterator(i);
+    next = it.next();
+    while (!next.done) {
+      q.push(matrix[next.value[0]][next.value[1]]);
+      next = it.next();
+    }
+    if (i % 2) {
+      q = q.slice(1).concat(q.slice(0, 1));
+    } else {
+      q = q.slice(-1).concat(q.slice(0, -1));
+    }
+    it = contourIterator(i);
+    next = it.next();
+    while (q.length) {
+      matrix[next.value[0]][next.value[1]] = q.shift();
+      next = it.next();
+    }
+  }
+
+  return matrix;
+
+  function* contourIterator(contour) {
+    let directions = [
+      ([i, j]) => [i, j + 1],
+      ([i, j]) => [i + 1, j],
+      ([i, j]) => [i, j - 1],
+      ([i, j]) => [i - 1, j],
+    ];
+    let startPos = [contour, contour];
+    let curPos = [contour, contour];
+    let curDirIndex = 0;
+    let curDir = directions[curDirIndex];
+    let nextPos = [];
+    while (nextPos[0] != startPos[0] || nextPos[1] !== startPos[1]) {
+      yield curPos;
+      nextPos = curDir(curPos);
+      if (
+        nextPos[0] < contour ||
+        nextPos[1] < contour ||
+        nextPos[0] >= matrix.length - contour ||
+        nextPos[1] >= matrix[0].length - contour
+      ) {
+        curDirIndex = (curDirIndex + 1) % 4;
+        curDir = directions[curDirIndex];
+        nextPos = curDir(curPos);
+      }
+      if (
+        nextPos[0] < contour ||
+        nextPos[1] < contour ||
+        nextPos[0] >= matrix.length - contour ||
+        nextPos[1] >= matrix[0].length - contour
+      ) {
+        break;
+      }
+      curPos = nextPos;
+    }
+  }
+}
+}

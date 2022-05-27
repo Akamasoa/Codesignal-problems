dc616ae638417e141c591de970bcebb9429c9918
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-05-27 20:21:10 -0300

    Update 112) Is Information Consistent?.js

--- Arcade/The Core/13) Waterfall of Integration/112) Is Information Consistent?.js
+++ Arcade/The Core/13) Waterfall of Integration/112) Is Information Consistent?.js
@@ -53,3 +53,11 @@ function helloWorld(name) {
 }
 */
 
+function solution(evidences) {
+let transpose = evidences[0].map((_, c) =>
+    evidences.map((_, i) => evidences[i][c])
+  );
+  return transpose.every(
+    (row) => Math.max(...row) <= 0 || Math.min(...row) >= 0
+  );
+}

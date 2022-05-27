ae822c0945bd2d001541d33331c804b351865edb
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-05-27 20:25:51 -0300

    Update 113) Correct Nonogram.js

--- Arcade/The Core/13) Waterfall of Integration/113) Correct Nonogram.js
+++ Arcade/The Core/13) Waterfall of Integration/113) Correct Nonogram.js
@@ -73,3 +73,90 @@ function helloWorld(name) {
 }
 */
 
+function solution(size, nonogramField) {
+let infoLength = Math.floor((size + 1) / 2);
+  let correct = true;
+
+  for (let i = infoLength; i < nonogramField.length; i++) {
+    let row = [];
+    let column = [];
+    let nextC;
+    let nextR;
+    let k;
+    let n;
+    for (let j = 0; j < infoLength; j++) {
+      nextC = nonogramField[j][i];
+      nextR = nonogramField[i][j];
+      if (nextC !== "-") {
+        column.push(Number(nextC));
+      }
+      if (nextR !== "-") {
+        row.push(Number(nextR));
+      }
+    }
+    k = infoLength;
+    while (correct && row.length) {
+      n = row.shift();
+      while (k < nonogramField.length && nonogramField[i][k] === ".") {
+        k++;
+      }
+      if (
+        n + k > nonogramField.length ||
+        (n > 0 && k == nonogramField.length)
+      ) {
+        correct = false;
+        break;
+      }
+      for (let h = 0; h < n; h++) {
+        if (nonogramField[i][k] !== "#") {
+          correct = false;
+        }
+        k++;
+      }
+      if (k !== nonogramField.length && nonogramField[i][k] === "#") {
+        correct = false;
+        break;
+      }
+    }
+    while (correct && k < nonogramField.length) {
+      if (nonogramField[i][k] === "#") {
+        correct = false;
+      }
+      k++;
+    }
+    k = infoLength;
+    while (correct && column.length) {
+      n = column.shift();
+      while (k < nonogramField.length && nonogramField[k][i] === ".") {
+        k++;
+      }
+      if (
+        n + k > nonogramField.length ||
+        (n > 0 && k == nonogramField.length)
+      ) {
+        correct = false;
+        break;
+      }
+      for (let h = 0; h < n; h++) {
+        if (nonogramField[k][i] !== "#") {
+          correct = false;
+        }
+        k++;
+      }
+      if (k !== nonogramField.length && nonogramField[k][i] === "#") {
+        correct = false;
+        break;
+      }
+    }
+    while (correct && k < nonogramField.length) {
+      if (nonogramField[k][i] === "#") {
+        correct = false;
+      }
+      k++;
+    }
+    if (!correct) {
+      break;
+    }
+  }
+  return correct;
+}

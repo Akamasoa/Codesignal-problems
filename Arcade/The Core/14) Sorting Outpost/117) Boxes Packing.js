bfae47b9589bc5b56e925aff00e3c084609cad54
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-05-29 21:46:52 -0300

    Update 117) Boxes Packing.js

--- Arcade/The Core/14) Sorting Outpost/117) Boxes Packing.js
+++ Arcade/The Core/14) Sorting Outpost/117) Boxes Packing.js
@@ -54,3 +54,63 @@ return "Hello, " + name;
 }
 */
 
+function solution(length, width, height) {
+ let isPosible = true;
+  for (let i = 0; i < length.length; i++) {
+    for (let j = 0; j < length.length; j++) {
+      if (i === j) {
+        continue;
+      }
+      if (
+        !(
+          (length[i] < length[j] &&
+            width[i] < width[j] &&
+            height[i] < height[j]) ||
+          (length[i] > length[j] &&
+            width[i] > width[j] &&
+            height[i] > height[j]) ||
+          (length[i] < length[j] &&
+            width[i] < height[j] &&
+            height[i] < width[j]) ||
+          (length[i] > length[j] &&
+            width[i] > height[j] &&
+            height[i] > width[j]) ||
+          (length[i] < height[j] &&
+            width[i] < width[j] &&
+            height[i] < length[j]) ||
+          (length[i] > height[j] &&
+            width[i] > width[j] &&
+            height[i] > length[j]) ||
+          (length[i] < width[j] &&
+            width[i] < length[j] &&
+            height[i] < height[j]) ||
+          (length[i] > width[j] &&
+            width[i] > length[j] &&
+            height[i] > height[j]) ||
+          (length[i] < height[j] &&
+            width[i] < length[j] &&
+            height[i] < width[j]) ||
+          (length[i] > height[j] &&
+            width[i] > length[j] &&
+            height[i] > width[j]) ||
+          (length[i] < width[j] &&
+            width[i] < height[j] &&
+            height[i] < length[j]) ||
+          (length[i] > width[j] &&
+            width[i] > height[j] &&
+            height[i] > length[j])
+        )
+      ) {
+        isPosible = false;
+      }
+
+      if (!isPosible) {
+        break;
+      }
+    }
+    if (!isPosible) {
+      break;
+    }
+  }
+  return isPosible;
+}

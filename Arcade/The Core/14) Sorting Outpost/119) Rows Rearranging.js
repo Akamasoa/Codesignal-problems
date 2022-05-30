6122b27456e9aba1f376a928bea3bfa3f6603ee1
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-05-30 18:57:22 -0300

    Create 119) Rows Rearranging.js

---
+++ Arcade/The Core/14) Sorting Outpost/119) Rows Rearranging.js
@@ -0,0 +1,46 @@
+/*
+Given a rectangular matrix of integers, check if it is possible to rearrange its rows in such a way that all its columns become strictly increasing sequences (read from top to bottom).
+
+Example
+
+For
+
+matrix = [[2, 7, 1], 
+          [0, 2, 0], 
+          [1, 3, 1]]
+the output should be
+solution(matrix) = false;
+
+For
+
+matrix = [[6, 4], 
+          [2, 2], 
+          [4, 3]]
+the output should be
+solution(matrix) = true.
+
+Input/Output
+
+[execution time limit] 4 seconds (js)
+
+[input] array.array.integer matrix
+
+A 2-dimensional array of integers.
+
+Guaranteed constraints:
+1 ≤ matrix.length ≤ 10,
+1 ≤ matrix[0].length ≤ 10,
+-300 ≤ matrix[i][j] ≤ 300.
+
+[output] boolean
+
+[JavaScript] Syntax Tips
+
+// Prints help message to the console
+// Returns a string
+function helloWorld(name) {
+    console.log("This prints to the console when you Run Tests");
+    return "Hello, " + name;
+}
+*/
+

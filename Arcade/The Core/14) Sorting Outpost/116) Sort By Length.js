7912cab31af986f1c6a45b0b14868914aba95d45
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-05-29 21:41:32 -0300

    Create 116) Sort By Length.js

---
+++ Arcade/The Core/14) Sorting Outpost/116) Sort By Length.js
@@ -0,0 +1,53 @@
+/*
+Given an array of strings, sort them in the order of increasing lengths. If two strings have the same length, their relative order must be the same as in the initial array.
+
+Example
+
+For
+
+
+inputArray = ["abc",
+              "",
+              "aaa",
+              "a",
+              "zz"]
+
+the output should be
+
+
+sortByLength(inputArray) = ["",
+                            "a",
+                            "zz",
+                            "abc",
+                            "aaa"]
+
+Input/Output
+
+[execution time limit] 4 seconds (js)
+
+[input] array.string inputArray
+
+A non-empty array of strings.
+
+Guaranteed constraints:
+3 ≤ inputArray.length ≤ 100,
+0 ≤ inputArray[i].length ≤ 100.
+
+[output] array.string
+
+[JavaScript (ES6)] Syntax Tips
+
+1
+2
+3
+4
+5
+6
+// Prints help message to the console
+// Returns a string
+function helloWorld(name) {
+console.log("This prints to the console when you Run Tests");
+return "Hello, " + name;
+}
+*/
+

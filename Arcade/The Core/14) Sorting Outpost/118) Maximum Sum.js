5a90e6e860473ff00b23c028c478ccfdd8eb147c
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-05-30 18:54:52 -0300

    Update 118) Maximum Sum.js

--- Arcade/The Core/14) Sorting Outpost/118) Maximum Sum.js
+++ Arcade/The Core/14) Sorting Outpost/118) Maximum Sum.js
@@ -48,3 +48,16 @@ return "Hello, " + name;
 }
 */
 
+function maximumSum(a, q) {
+  a.sort((b, c) => b - c);
+  let counts = a.map((b, i) => ({
+    i,
+    c: q.reduce((acc, query) => acc + (i >= query[0] && i <= query[1]), 0),
+  }));
+  counts.sort((b, c) => c.c - b.c);
+  let solution = Array(counts.length);
+  for (let i = 0; i < counts.length; i++) {
+    solution[counts[i].i] = a.pop() * counts[i].c;
+  }
+  return solution.reduce((b, c) => b + c);
+}

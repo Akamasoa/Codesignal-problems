2a6e5ac534d1e379c3249938a65f0db615d06907
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-06-01 22:26:11 -0300

    Update 124) Bishop Diagonal.js

--- Arcade/The Core/15) Chess Tavern/124) Bishop Diagonal.js
+++ Arcade/The Core/15) Chess Tavern/124) Bishop Diagonal.js
@@ -52,3 +52,50 @@ function helloWorld(name) {
 }
 */
 
+function solution(bishop1, bishop2) {
+let bishopCoords1 = bishop1.split("");
+  let bishopCoords2 = bishop2.split("");
+  let tmp;
+  bishopCoords1[0] = 1 + bishopCoords1[0].charCodeAt(0) - "a".charCodeAt(0);
+  bishopCoords1[1] = Number(bishopCoords1[1]);
+  bishopCoords2[0] = 1 + bishopCoords2[0].charCodeAt(0) - "a".charCodeAt(0);
+  bishopCoords2[1] = Number(bishopCoords2[1]);
+  let newPos1 = bishopCoords1.slice();
+  let newPos2 = bishopCoords2.slice();
+  if (
+    bishopCoords1[0] + bishopCoords1[1] ===
+    bishopCoords2[0] + bishopCoords2[1]
+  ) {
+    if (bishopCoords1[0] + bishopCoords1[1] > 9) {
+      newPos1[0] = 8;
+      newPos2[1] = 8;
+      newPos1[1] = bishopCoords1[0] + bishopCoords1[1] - 8;
+      newPos2[0] = bishopCoords1[0] + bishopCoords1[1] - 8;
+    } else {
+      newPos1[0] = 1;
+      newPos2[1] = 1;
+      newPos1[1] = bishopCoords1[0] + bishopCoords1[1] - 1;
+      newPos2[0] = bishopCoords1[0] + bishopCoords1[1] - 1;
+    }
+  } else if (
+    bishopCoords1[0] - bishopCoords1[1] ===
+    bishopCoords2[0] - bishopCoords2[1]
+  ) {
+    if (bishopCoords1[0] - bishopCoords1[1] < 0) {
+      newPos1[0] = 1;
+      newPos1[1] = 1 + bishopCoords1[1] - bishopCoords1[0];
+      newPos2[0] = 8 + bishopCoords1[0] - bishopCoords1[1];
+      newPos2[1] = 8;
+    } else {
+      newPos1[0] = 1 + bishopCoords1[0] - bishopCoords1[1];
+      newPos1[1] = 1;
+      newPos2[0] = 8;
+      newPos2[1] = 8 - (bishopCoords1[0] - bishopCoords1[1]);
+    }
+  }
+  newPos1[0] = String.fromCharCode(newPos1[0] - 1 + "a".charCodeAt(0));
+  newPos1 = newPos1.join("");
+  newPos2[0] = String.fromCharCode(newPos2[0] - 1 + "a".charCodeAt(0));
+  newPos2 = newPos2.join("");
+  return [newPos1, newPos2].sort();
+}

389df2a5436de9ab9a4969e982b45050afce53da
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-06-01 22:23:31 -0300

    Update 123) Chess Knight Moves.js

--- Arcade/The Core/15) Chess Tavern/123) Chess Knight Moves.js
+++ Arcade/The Core/15) Chess Tavern/123) Chess Knight Moves.js
@@ -50,3 +50,21 @@ return "Hello, " + name;
 }
 */
 
+function solution(cell) {
+let posibleMoves = ([i, j]) => [
+    [i - 2, j - 1],
+    [i - 1, j - 2],
+    [i - 2, j + 1],
+    [i - 1, j + 2],
+    [i + 2, j - 1],
+    [i + 1, j - 2],
+    [i + 2, j + 1],
+    [i + 1, j + 2],
+  ];
+  cellCoords = cell.split("");
+  cellCoords[0] = 1 + cellCoords[0].charCodeAt(0) - "a".charCodeAt(0);
+  cellCoords[1] = Number(cellCoords[1]);
+  return posibleMoves(cellCoords).filter(
+    (c) => c[0] > 0 && c[0] < 9 && c[1] > 0 && c[1] < 9
+  ).length;
+}

c287d856ecc23fa675f49e83ef71971110c3c1c4
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-06-02 22:27:47 -0300

    Update 126) Chess Bishop Dream.js

--- Arcade/The Core/15) Chess Tavern/126) Chess Bishop Dream.js
+++ Arcade/The Core/15) Chess Tavern/126) Chess Bishop Dream.js
@@ -62,3 +62,29 @@ function helloWorld(name) {
 }
 */
 
+function solution(boardSize, initPosition, initDirection, k) {
+let verticalSteps =
+    (initDirection[0] === 1
+      ? initPosition[0]
+      : boardSize[0] - initPosition[0] - 1) + k;
+  let horizontalSteps =
+    (initDirection[1] === 1
+      ? initPosition[1]
+      : boardSize[1] - initPosition[1] - 1) + k;
+  let verticalBoards = Math.floor(verticalSteps / boardSize[0]) % 2;
+  let horizontalBoards = Math.floor(horizontalSteps / boardSize[1]) % 2;
+  verticalBoards = (verticalBoards + (initDirection[0] === -1 ? 1 : 0)) % 2;
+  horizontalBoards = (horizontalBoards + (initDirection[1] === -1 ? 1 : 0)) % 2;
+
+  let lastPosition = [
+    verticalSteps % boardSize[0],
+    horizontalSteps % boardSize[1],
+  ];
+  if (verticalBoards === 1) {
+    lastPosition[0] = boardSize[0] - lastPosition[0] - 1;
+  }
+  if (horizontalBoards === 1) {
+    lastPosition[1] = boardSize[1] - lastPosition[1] - 1;
+  }
+  return lastPosition;
+}

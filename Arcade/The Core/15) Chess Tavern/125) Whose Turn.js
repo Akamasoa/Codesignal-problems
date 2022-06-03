f83598b19f1b6c48fe453d5003d2d9a8851ebfa3
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-06-02 22:25:57 -0300

    Update 125) Whose Turn.js

--- Arcade/The Core/15) Chess Tavern/125) Whose Turn.js
+++ Arcade/The Core/15) Chess Tavern/125) Whose Turn.js
@@ -41,3 +41,17 @@ function helloWorld(name) {
 }
 */
 
+function solution(p) {
+p = p.split(";").map((k) => getPosition(k));
+  return (
+    (p[0][0] + p[0][1] + p[1][0] + p[1][1]) % 2 ===
+    (p[2][0] + p[2][1] + p[3][0] + p[3][1]) % 2
+  );
+}
+
+function getPosition(k) {
+  let pos = k.split("");
+  pos[0] = 1 + pos[0].charCodeAt(0) - "a".charCodeAt(0);
+  pos[1] = Number(pos[1]);
+  return pos;
+}

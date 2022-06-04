7134c2b92f21b9ea567068f60920683884e419d7
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-06-03 23:38:31 -0300

    Create 127) Chess Triangle.js

---
+++ Arcade/The Core/15) Chess Tavern/127) Chess Triangle.js
@@ -0,0 +1,41 @@
+/*
+Consider a bishop, a knight and a rook on an n × m chessboard. They are said to form a triangle if each piece attacks exactly one other piece and is attacked by exactly one piece. Calculate the number of ways to choose positions of the pieces to form a triangle.
+
+Note that the bishop attacks pieces sharing the common diagonal with it; the rook attacks in horizontal and vertical directions; and, finally, the knight attacks squares which are two squares horizontally and one square vertically, or two squares vertically and one square horizontally away from its position.
+
+
+
+Example
+
+For n = 2 and m = 3, the output should be
+solution(n, m) = 8.
+
+
+
+Input/Output
+
+[execution time limit] 4 seconds (js)
+
+[input] integer n
+
+Guaranteed constraints:
+1 ≤ n ≤ 40.
+
+[input] integer m
+
+Guaranteed constraints:
+1 ≤ m ≤ 40,
+3 ≤ n · m.
+
+[output] integer
+
+[JavaScript] Syntax Tips
+
+// Prints help message to the console
+// Returns a string
+function helloWorld(name) {
+    console.log("This prints to the console when you Run Tests");
+    return "Hello, " + name;
+}
+*/
+

af6be9dbcf94a71f44857498dcb7eb9b392f0b71
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-06-08 19:46:55 -0300

    Update 134) New Year Celebrations.js

--- Arcade/The Core/16) Time River/134) New Year Celebrations.js
+++ Arcade/The Core/16) Time River/134) New Year Celebrations.js
@@ -63,3 +63,36 @@ function helloWorld(name) {
 }
 */
 
+function solution(takeOffTime, minutes) {
+if (takeOffTime === "00:00") {
+    takeOffTime = "2019-01-01 " + takeOffTime;
+  } else {
+    takeOffTime = "2018-12-31 " + takeOffTime;
+  }
+  let celebrations = 0;
+  let date = new Date();
+  let accuTime = 0;
+  date.setTime(Date.parse(takeOffTime));
+  let year = date.getFullYear();
+  let lastYear = year;
+  for (let i = 0; i < minutes.length; i++) {
+    minutes[i] -= accuTime;
+    if (date.getHours() === 0 && date.getMinutes() === 0) {
+      celebrations++;
+    }
+    date.setMinutes(date.getMinutes() + minutes[i]);
+    if (lastYear < date.getFullYear()) {
+      celebrations++;
+    }
+    date.setHours(date.getHours() - 1);
+    lastYear = date.getFullYear();
+    accuTime += minutes[i];
+  }
+  if (
+    date.getFullYear() === 2018 ||
+    (date.getHours() === 0 && date.getMinutes() === 0)
+  ) {
+    celebrations++;
+  }
+  return celebrations;
+}

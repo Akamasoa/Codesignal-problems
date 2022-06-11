76284afbb38df5c7c83a498fd4ef2a1fd0466862
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-06-10 23:22:33 -0300

    Create 139) Replace All Digits RegExp.js

---
+++ Arcade/The Core/17) Regular Hell/139) Replace All Digits RegExp.js
@@ -0,0 +1,30 @@
+/*
+Implement the missing code, denoted by ellipses. You may not modify the pre-existing code.
+Implement a function that replaces each digit in the given string with a '#' character.
+
+Example
+
+For input = "There are 12 points", the output should be
+solution(input) = "There are ## points".
+
+Input/Output
+
+[execution time limit] 4 seconds (js)
+
+[input] string input
+
+Guaranteed constraints:
+5 ≤ input.length ≤ 20.
+
+[output] string
+
+[JavaScript] Syntax Tips
+
+// Prints help message to the console
+// Returns a string
+function helloWorld(name) {
+    console.log("This prints to the console when you Run Tests");
+    return "Hello, " + name;
+}
+*/
+
